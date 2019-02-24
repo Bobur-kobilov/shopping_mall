26eae6310875f640c4d1ebf7c225b968ea6554c8
Boburjon Kobilov <boburjonkobilov@Boburjons-MacBook-Air.local>
2019-02-24 18:23:35 +0900

    minor change

--- backendApp/sql/productQuery.js
+++ backendApp/sql/productQuery.js
@@ -2,6 +2,7 @@ function selInfo () {
   return `
   SELECT 
     product_id as productId,
+    product_desc as productDesc
     picture as productImage,
     product_name as productName,
     price as productPrice
