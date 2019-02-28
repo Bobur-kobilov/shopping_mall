a8736410e4389f2b8ccbcc60fe6023ea1afc7995
Boburjon Kobilov <boburjonkobilov@Boburjons-MacBook-Air.local>
2019-02-28 21:55:42 +0900

    wallet added

--- reactShoppingMall/src/axios.js
+++ reactShoppingMall/src/axios.js
@@ -2,5 +2,6 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: "http://ec2-52-55-242-221.compute-1.amazonaws.com:2323"
+  // baseURL: "http://localhost:2323"
 });
 export default instance;
